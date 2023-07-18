0fbc5dd419439b4f9edc87562dfb60fa3a145e4e
oguzhansecgel <oguzhansecgel19@icloud.com>
2023-07-18 19:29:38 +0300

    Identity işlemleri yapıldı. Giriş ve Kayıt Ekranı Tamamlandı.

---
+++ Frontend/HotelProject.WebUI/Dtos/RegisterDto/CreateNewUserDto.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+namespace HotelProject.WebUI.Dtos.RegisterDto
+{
+	public class CreateNewUserDto
+	{
+		[System.ComponentModel.DataAnnotations.Required(ErrorMessage="Ad Alanı Gereklidir..!")]
+		public string Name { get; set; }
+
+		[System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Soyadı Alanı Gereklidir..!")]
+		public string SurName { get; set; }
+
+		[System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Kullanıcı Adı Alanı Gereklidir..!")]
+		public string UserName { get; set; }
+		public string City { get; set; }
+
+		[System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Mail Alanı Gereklidir..!")]
+		public string Mail { get; set; }
+
+		[System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Şifre Alanı Gereklidir..!")]
+		public string Password { get; set; }
+
+		[System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Şifre Tekrar Alanı Gereklidir..!")]
+		[Compare("Password",ErrorMessage ="Şifreler Uyuşmuyor")]
+		public string ConfirmPassword { get; set; }
+	}
+}
--- Frontend/HotelProject.WebUI/Program.cs
+++ Frontend/HotelProject.WebUI/Program.cs
@@ -1,7 +1,16 @@
+using HotelProject.DataAccessLayer.Concrete;
+using HotelProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.DataProtection;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddAutoMapper(typeof(Program));
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDataProtection().PersistKeysToFileSystem(new DirectoryInfo(Path.GetTempPath()));
+builder.Services.AddDbContext<Context>();
+builder.Services.AddIdentity<AppUser,AppRole>().AddEntityFrameworkStores<Context>();
+
 
 builder.Services.AddHttpClient();
 var app = builder.Build();
